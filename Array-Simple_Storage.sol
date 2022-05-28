1cbcb68ec8b187286ad209f2a3782478fd4a20a4
CubeForm <cubeform@icloud.com>
2022-05-29 00:39:23 +0200

    Update Array-Simple_Storage.sol

--- Array-Simple_Storage.sol
+++ Array-Simple_Storage.sol
@@ -4,22 +4,22 @@ pragma solidity 0.8.8;
 
 
 // My version of SimpleStorage Smart contract Sample have an array of animal owner's name and their animal
-// with a function to store both values and a function to retrieve them
+// with a function to store both values and a mapping to retrieve the animal from the owner's name
 
 contract SimpleStorage {
     
     struct AnimalOwner {
-       
+        
         string Name;
         string Animal;
     }                 
 
-    AnimalOwner[] public animalowner;                                // [] this is an array
-    
+    AnimalOwner[] public  animalowner;                                // [] this is an array
+    mapping (string => string ) public OwnerToAnimal;                 // with mapping we can link a owner's name to his animal
     
     function AddOwner(string memory _Name, string memory _Animal) public { 
         animalowner.push(AnimalOwner(_Name, _Animal));
+        OwnerToAnimal[_Name] = _Animal;
     }                                                               // this function store the values in the array
-    function retrieve() public view returns(AnimalOwner memory) {}  // this function retrive the values from the array
 
 }
